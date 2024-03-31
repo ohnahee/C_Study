37525b9c735cecebf301f0d4299816fa2577f143
ohnhaee <ohnahee0824@gmail.com>
2024-04-01 08:29:38 +0900

    특수문자 출력

---
+++ 1 week/10172.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//  // 가 /로 출력됨
+//  ["]출력시 [\"] 형태로 바꾸어주어야 출력됨
+
+int main(){
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( 0 )\"\"\"\\\n");
+    printf("|\"^\"`    |\n");
+    printf("||_/=\\\\__|");
+    return 0;
+}
