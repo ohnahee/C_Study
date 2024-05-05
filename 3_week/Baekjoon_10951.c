1bd4a6c84d760c6fcb453a60404deaf589ee2381
ohnahee <53605414+ohnahee@users.noreply.github.com>
2024-05-05 14:54:34 +0000

    10951 commit

---
+++ 3_week/Baekjoon_10951.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    while (1) {
+
+        if (scanf("%d %d", &a, &b) != 2) {
+            break; 
+        }
+        
+        printf("%d\n", a + b);
+    }
+
+    return 0;
+}
